18d32d97e8ac120dfd5b3fc58c827ac8266205ce
Rick <rickduyhuynh98@gmail.com>
2018-06-29 14:51:00 -0700

    no mroe

--- keys.js
+++
@@ -1,8 +0,0 @@
-console.log('this is loaded');
-
-exports.twitterKeys = {
-  consumer_key: 'tEJYDOP2jmbbRl85EJIWrWXUX',
-  consumer_secret: '6HZ1VXWywlSCd0qaeQSluxmQMRo3P56vp1gybEYRoYxlhQkxEX',
-  access_token_key: '1012426407853428736-nx8oiXt2aiQEY36lLG1Dz9239tMuiH',
-  access_token_secret: 'LOJ30Yu9nH6Szn5VcuRVEif1zVBMJOxjoy64StUWtSWnj',
-}
